dad1fd16c61a99190c90675e3f5cf651e42fa33d
Gouthum Karadi <karadi@virtualnex.us>
2021-12-12 17:17:58 -0400

    Add html

--- react-dapp/src/App.js
+++ react-dapp/src/App.js
@@ -26,23 +26,26 @@ function App() {
     }
   }
    async function setGreeting() {
-
+    if (!greeting) return
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount()
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer)
+      const transaction = await contract.setGreeting(greeting)
+      await transaction.wait()
+      fetchGreeting()
+    }
    } 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={fetchGreeting}>Fetch Greeting</button>
+        <button onClick={setGreeting}>Set Greeting</button>
+        <input 
+          onChange={e => setGreetingValue(e.target.value)}
+          placeholder="Set Greeting"
+        />
       </header>
     </div>
   );
