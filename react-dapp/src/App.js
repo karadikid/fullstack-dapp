ea60b6946c88b61f33d8bb7c01ac5b74fe2ae175
Gouthum Karadi <karadi@virtualnex.us>
2021-12-12 19:26:22 -0400

    It works, reset metamask account in Advanced settings if too many transactions per account in mining pool on localhost.

--- react-dapp/src/App.js
+++ react-dapp/src/App.js
@@ -5,7 +5,7 @@ import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
 import Token from './artifacts/contracts/token.sol/Token.json'
 
 const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-const tokenAddress = "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512"
+const tokenAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
 
 function App() {
   const [greeting, setGreetingValue] = useState(' ')
@@ -58,7 +58,7 @@ function App() {
       await requestAccount()
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner();
-      const contract = new ethers.Contract(tokenAddress, Token.abi, provider);
+      const contract = new ethers.Contract(tokenAddress, Token.abi, signer);
       const transaction = await contract.transfer(userAccount, amount);
       await transaction.wait();
       console.log(`${amount} Coins successfully sent to ${userAccount}`);
