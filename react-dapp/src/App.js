a1f5d7099535da2318be3091a34c1da043aa578b
Gouthum Karadi <karadi@virtualnex.us>
2021-12-12 18:43:28 -0400

    Added Token Transactions.

--- react-dapp/src/App.js
+++ react-dapp/src/App.js
@@ -1,21 +1,47 @@
 import './App.css';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { ethers } from 'ethers';
 import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
-import { EtherscanProvider } from '@ethersproject/providers';
+import Token from './artifacts/contracts/token.sol/Token.json'
 
 const greeterAddress = "0xA0D8e657c03CeDDeE7d2d1eC506CCa200690c2bE"
+const tokenAddress = "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512"
 
 function App() {
   const [greeting, setGreetingValue] = useState(' ')
-  //
+  const [userAccount, setUserAccount] = useState(' ')
+  const [amount, setAmount] = useState(0)
+  
   async function requestAccount() {
     await window.ethereum.request({ method: 'eth_requestAccounts'});
   }
+
+async function getBalance( {
+  if (typeof window.ethereum !== 'undefined') {
+    const [account] = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider);
+    const balance = await contract.balanceOf (account);
+    console.log("Balance: ", balance.toString());
+  }
+}
+  
+  async function sendCoins(){
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount()
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(tokenAddress, Token.abi, provider);
+      const transaction = await contract.transfer(userAccount, amount);
+      await transaction.wait();
+      console.log(`${amount} Coins successfully sent to ${userAccount}`);
+    }
+  }
+
   async function fetchGreeting() {
     if (typeof window.ethereum !== 'undefined') {
       const provider = new ethers.providers.Web3Provider(window.ethereum)
-      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+      const contract = new ethers.Contract(tokenAddress, Token.abi, provider)
       try {
           const data = await contract.greet()
           console.log('data: ', data)
