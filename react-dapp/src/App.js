039c918223e6d312668abbb4d213f456482894b6
Gouthum Karadi <karadi@virtualnex.us>
2021-12-12 17:08:32 -0400

    Added App.js code and pushed contract.

--- react-dapp/src/App.js
+++ react-dapp/src/App.js
@@ -1,7 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { ethers } from 'ethers';
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
+import { EtherscanProvider } from '@ethersproject/providers';
+
+const greeterAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3"
 
 function App() {
+  const [greeting, setGreetingValue] = useState()
+  //
+  async function requestAccount() {
+    await window.ethereum.request({ method: 'eth_requestAccounts'});
+  }
+  async function fetchGreeting() {
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+      try {
+          const data = await contract.greet()
+          console.log('data: ', data)
+      } catch (err) {
+        console.log("Error: ", err)
+      }
+    }
+  }
+   async function setGreeting() {
+
+   } 
   return (
     <div className="App">
       <header className="App-header">
